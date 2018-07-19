587d10171ba9c56a961e685cf1828bd0786c27cf
Maxym Dyachenko <max.dyachenko@inbox.ru>
2018-07-19 19:33:56 +0300

    add ad-block

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import './styles/fonts/catamaran.styl';
 import './styles/fonts/gotham.styl';
 import './styles/fonts/playfair.styl';
 import 'font-awesome/css/font-awesome.min.css'
+import 'linearicons'
 
